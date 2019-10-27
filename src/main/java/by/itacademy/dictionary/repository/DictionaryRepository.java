9aeecd28459e472f655a5eba6189e019b67294ba
RyhorPaprauko <ryhorpaprauka@mail.com>
2019-10-27 11:05:40 +0300

    corrections

--- src/main/java/by/itacademy/dictionary/repository/DictionaryRepository.java
+++ src/main/java/by/itacademy/dictionary/repository/DictionaryRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class DictionaryRepository {
 
-    private static final String FULL_QUERY = "select dictionary_2.word, dictionary_2.reference_id " +
-            "from dictionary_2 left join dictionary_1 " +
-            "on dictionary_2.word = dictionary_1.word  where dictionary_1.word is null " +
-            "union select * from dictionary_1";
+    private static final String FULL_QUERY = "select dictionary_2.word, dictionary_2.reference_id "
+            + "from dictionary_2 left join dictionary_1 "
+            + "on dictionary_2.word = dictionary_1.word  where dictionary_1.word is null "
+            + "union select * from dictionary_1";
     private static final String FIRST_TABLE_QUERY = "select * from dictionary_1";
     private static final String SECOND_TABLE_QUERY = "select * from dictionary_2";
     private final JdbcTemplate jdbcTemplate;
